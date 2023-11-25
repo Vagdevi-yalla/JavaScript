8af59c904a93f4b68bb68364117b6f8eec4e0f83
Vagdevi-yalla <138780169+Vagdevi-yalla@users.noreply.github.com>
2023-11-25 07:32:58 +0000

    DataTypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // this treats all JS code as newer version
+
+alert(3+7) // this wont work because we are using node not browser
+
+console.log(3
+    +
+    
+    4) // code readable should be high
+
+    //types of datatypes
+    //primitive
+    //number => 2 to the power 53
+    //bigint
+    //string =>""
+    //boolean => true/false
+    //null => standalone value
+    //undefined =>
+    //symbol => unique
+
+    //non-primitive
+
+    //object
